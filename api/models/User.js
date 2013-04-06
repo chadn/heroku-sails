60b5b224ff01ec47535f20667d3af716c8b9085b
Chad Norwood <chad@chadnorwood.com>
2013-04-06 12:24:23 -0500

    sails generate model User

---
+++ api/models/User.js
@@ -0,0 +1,20 @@
+/*---------------------
+	:: User
+	-> model
+---------------------*/
+module.exports = {
+
+	attributes	: {
+
+		// Simple attribute:
+		// name: 'STRING',
+
+		// Or for more flexibility:
+		// phoneNumber: {
+		//	type: 'STRING',
+		//	defaultValue: '555-555-5555'
+		// }
+		
+	}
+
+};
